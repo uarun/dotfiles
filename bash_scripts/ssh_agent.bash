0eff9dacb0f404c6b1ea5860305c3bda052f0917
Arun Udayashankar <arunkumar.u@gmail.com>
2012-07-13 00:30:47 -0400

    Tweaked new ssh-agent startup message

--- bash_scripts/ssh_agent.bash
+++ bash_scripts/ssh_agent.bash
@@ -1,9 +1,9 @@
 SSH_ENV="$HOME/.ssh/environment"
 
 function start_agent {
-     echo "Initializing new SSH agent..."
+     echo -n "Initializing new SSH agent ... "
      /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-     echo succeeded
+     echo "Done"
      chmod 600 "${SSH_ENV}"
      . "${SSH_ENV}" > /dev/null
      /usr/bin/ssh-add;
