0379de59408f3ed0b8c91f66997e85e7e2cd8496
Arun Udayashankar <arunkumar.u@gmail.com>
2012-07-14 13:27:44 -0400

    Moved contents of zprofile to end of zshrc (for correct ordering)

--- common/aliases.sh
+++ common/aliases.sh
@@ -6,11 +6,11 @@ alias gst='git status'
 alias glg='git log --graph --pretty=oneline --abbrev-commit --all --decorate'
 
 # Auto complete for 'g' as well
-complete -o default -o nospace -F _git g
+# complete -o default -o nospace -F _git g
 
-unalias gl > /dev/null
-unalias gm > /dev/null
-unalias gp > /dev/null
+unalias gl
+unalias gm
+unalias gp
 
 ## ===== General ===== ###
 alias cls=clear
