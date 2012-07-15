6841eb52734bebe75c250393498beb34bdb2eb55
Arun Udayashankar <audayashankar@liquidnet.com>
2012-07-14 23:27:06 -0400

    Redirect unalias errors to /dev/null

--- common/aliases.sh
+++ common/aliases.sh
@@ -8,9 +8,9 @@ alias glg='git log --graph --pretty=oneline --abbrev-commit --all --decorate'
 # Auto complete for 'g' as well
 # complete -o default -o nospace -F _git g
 
-unalias gl
-unalias gm
-unalias gp
+unalias gl 2> /dev/null
+unalias gm 2> /dev/null
+unalias gp 2> /dev/null
 
 ## ===== General ===== ###
 alias cls=clear
