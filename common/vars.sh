1a8619c95be7be40a8a9270305894760e75c3d95
Arun Udayashankar <audayashankar@liquidnet.com>
2012-07-16 19:30:21 -0400

    Use the default TERM settings

--- common/vars.sh
+++ common/vars.sh
@@ -1,7 +1,7 @@
 # Set TERM to xterm-256color if not invoked under tmux
-case "$TERM" in
-    screen|screen-256color)
-        ;;
-    *)
-        export TERM=xterm-256color;;
-esac
+#case "$TERM" in
+#    screen|screen-256color)
+#        ;;
+#    *)
+#        export TERM=xterm-256color;;
+#esac
