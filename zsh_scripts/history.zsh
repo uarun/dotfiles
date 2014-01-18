9799835ab96b6622e61ab873f603f45df8ccbf91
Arun Udayashankar <arunkumar.u@gmail.com>
2014-01-17 23:42:01 -0500

    Bumped up history size

--- zsh_scripts/history.zsh
+++ zsh_scripts/history.zsh
@@ -1,8 +1,8 @@
 # ===== History Configuration ===== #
 export HISTIGNORE="&:ls:[bf]g:exit:reset:clear:cd:cd ..:cd.."
 export HISTFILE=~/.zsh_history
-export HISTSIZE=15000
-export SAVEHIST=10000
+export HISTSIZE=150000
+export SAVEHIST=150000
 setopt INC_APPEND_HISTORY
 setopt HIST_IGNORE_ALL_DUPS
 setopt HIST_IGNORE_SPACE
